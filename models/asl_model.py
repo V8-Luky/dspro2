5eb04c4b4c1f28a1ee15b3f5c4c45831e4f6d84f
V8-Luky <45991846+V8-Luky@users.noreply.github.com>
2025-04-09 20:12:31 +0200

    Add training procedure with experiment tracking (#31)

---
+++ models/asl_model.py
@@ -0,0 +1,59 @@
+import lightning as L
+
+import torchmetrics
+import torchmetrics.classification
+
+class ASLModel(L.LightningModule):
+    TRAIN_ACCURACY = "train_accuracy"
+    VALID_ACCURACY = "valid_accuracy"
+    TEST_ACCURACY = "test_accuracy"
+    TRAIN_LOSS = "train_loss"
+    VALID_LOSS = "valid_loss"
+    TEST_LOSS = "test_loss"
+
+    def __init__(self, model, criterion, optimizer, lr_scheduler=None):
+        super().__init__()
+
+        self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.lr_scheduler = lr_scheduler
+
+        self.train_accuracy = torchmetrics.classification.MulticlassAccuracy(num_classes=28, average="micro")
+        self.valid_accuracy = torchmetrics.classification.MulticlassAccuracy(num_classes=28, average="micro")
+        self.test_accuracy = torchmetrics.classification.MulticlassAccuracy(num_classes=28, average="micro")
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch, batch_idx):
+        y_hat, loss = self._handle_step(batch, batch_idx, self.train_accuracy, ASLModel.TRAIN_LOSS, ASLModel.TRAIN_ACCURACY)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        self._handle_step(batch, batch_idx, self.valid_accuracy, ASLModel.VALID_LOSS, ASLModel.VALID_ACCURACY)
+
+    def test_step(self, batch, batch_idx):
+        self._handle_step(batch, batch_idx, self.test_accuracy, ASLModel.TEST_LOSS, ASLModel.TEST_ACCURACY)
+
+    def predict_step(self, batch, batch_idx):
+        x, _ = batch
+        y_hat = self(x)
+        return y_hat
+
+    def _handle_step(self, batch, batch_idx: int, metric: torchmetrics.Metric, loss_name: str, metric_name: str):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.criterion(y_hat, y)
+
+        metric(y_hat, y)
+
+        self.log(loss_name, loss, on_step=True, on_epoch=True)
+        self.log(metric_name, metric, on_step=True, on_epoch=True)
+
+        return y_hat, loss
+
+    def configure_optimizers(self):
+        if self.lr_scheduler:
+            return [self.optimizer], [self.lr_scheduler]
+        return self.optimizer
---
+++ models/training.py
@@ -0,0 +1,69 @@
+import lightning as L
+import wandb
+
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint, EarlyStopping
+
+from .asl_model import ASLModel
+
+ENTITY_NAME = "dspro2-silent-speech"
+PROJECT_NAME = "silent-speech"
+
+MAX_EPOCHS = 200
+
+
+def train(model: ASLModel, datamodule: L.LightningDataModule, logger: WandbLogger, callbacks: list[L.Callback] = None, seed: int = 42):
+    """Trains the model using the given datamodule and logger.
+    Args:
+        model (ASLModel): The model to train.
+        datamodule (L.LightningDataModule): The datamodule containing the training and validation data.
+        logger (WandbLogger): The logger to use for logging metrics.
+        callbacks (list[L.Callback], optional): List of callbacks to use during training. Defaults to None.
+        seed (int, optional): Seed for reproducibility. Defaults to 42.
+    """
+
+    L.seed_everything(seed)
+
+    all_callbacks = get_default_callbacks()
+    if callbacks is not None:
+        all_callbacks.extend(callbacks)
+
+    trainer = L.Trainer(
+        accelerator="auto",
+        devices="auto",
+        max_epochs=MAX_EPOCHS,
+        logger=logger,
+        callbacks=all_callbacks
+    )
+
+    trainer.fit(model, datamodule=datamodule)
+
+    for checkpoint in filter(lambda c: isinstance(c, ModelCheckpoint), all_callbacks):
+        artifact = wandb.Artifact(name=checkpoint.monitor, description=f"{checkpoint.monitor} checkpoints", type="model")
+        artifact.add_dir(checkpoint.dirpath)
+        wandb.run.log_artifact(artifact)
+
+    wandb.finish()
+
+
+def get_default_callbacks():
+    return [
+        LearningRateMonitor(logging_interval="step", log_momentum=True, log_weight_decay=True),
+        ModelCheckpoint(monitor=ASLModel.VALID_ACCURACY, filename="{epoch:02d}-{valid_accuracy:.2f}", save_top_k=3, mode="max"),
+        EarlyStopping(monitor=ASLModel.VALID_ACCURACY, patience=5, verbose=True, mode="max"),
+        EarlyStopping(monitor=ASLModel.TRAIN_ACCURACY, patience=5, verbose=True, mode="max"),
+    ]
+
+
+def sweep(sweep_config: dict, count: int, training_procedure):
+    """Starts a W&B sweep with the given configuration and count.
+
+    Args:
+        sweep_config (dict): The configuration for the sweep.
+        count (int): The number of runs to execute.
+        training_procedure (function): The function to run for each sweep run. Should setup the model for the training based on the sweep configuratio accessible by wandb.config.
+    """
+
+    sweep_id = wandb.sweep(sweep=sweep_config, project=PROJECT_NAME, entity=ENTITY_NAME)
+    wandb.agent(sweep_id=sweep_id, function=training_procedure, count=count)
+    wandb.teardown()
