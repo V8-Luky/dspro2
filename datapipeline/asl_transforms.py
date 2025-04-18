675ab0428bdbc1714bc767039710344e19a56653
Luca Kyburz <kyburzluca@gmail.com>
2025-04-18 18:01:05 +0200

    Finally - a working and clean solution for the transforms

--- datapipeline/asl_transforms.py
+++ datapipeline/asl_transforms.py
@@ -6,86 +6,79 @@ import torchvision.transforms.v2 as transforms
 from PIL import Image
 
 
-class RandomBackgroundTransform(nn.Module):
-    """Base class for random background transformations combined with a random color jitter of the hand in the foreground."""
+def _get_mask(img, hsv_min, hsv_max):
+    """Gets a mask for the background on the image."""
+    image_hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+    return cv.inRange(image_hsv, hsv_min, hsv_max)
 
-    def __init__(self, brightness=0.75, contrast=0.3, saturation=0.75, hue=0.4):
-        super().__init__()
-        self.color_jitter = transforms.ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation, hue=hue)
+
+class ExtractHand(nn.Module):
+    """Extracts the hand from the image using a mask based on HSV color space.
+
+    This transform should only be used on images obtained from the https://www.kaggle.com/datasets/kapillondhe/american-sign-language 
+    dataset.
+    """
 
     def forward(self, img):
-        """Applies a color jitter to the hand in the foreground and replaces the white background with a new background."""
-
-        transformed_foreground = np.array(self._apply_foreground_transform(img))
-
         img_np = np.array(img)
         hand_filter = self._get_hand_filter(img_np)
-
-        transformed_img_with_background = self._apply_background(transformed_foreground, hand_filter)
-
-        return Image.fromarray(transformed_img_with_background)
-
-    def _apply_foreground_transform(self, img):
-        """Applies a random color jitter to the hand in the foreground."""
-
-        return self.color_jitter(img)
-
-    def _apply_background(self, img, mask):
-        """Applies a random background to the image based on the mask. Where everything in the mask is the foreground."""
-
-        raise NotImplementedError("This method should be overridden in subclasses.")
+        hand_only_image = self._apply_mask(img_np, hand_filter)
+        return Image.fromarray(hand_only_image)
 
     def _get_hand_filter(self, img):
         """Gets a mask for the hand on the image."""
+        bg_mask = _get_mask(img, np.array([0, 0, 100]), np.array([180, 60, 255]))
+        hand_mask = cv.bitwise_not(bg_mask)
+        return cv.GaussianBlur(hand_mask, (3, 3), 3)
 
-        image_hsv = cv.cvtColor(img, cv.COLOR_RGB2HSV)
+    def _apply_mask(self, img, mask):
+        """Applies a random noise background to the image based on the mask."""
 
-        hsv_min = [np.array([0, 0, 100])]
-        hsv_max = [np.array([180, 60, 255])]
+        black = np.zeros_like(img)
+        mask = np.expand_dims(mask, -1)
 
-        hsv_min, hsv_max = np.array(hsv_min), np.array(hsv_max)
-
-        background_image_hsv = sum([cv.inRange(image_hsv, lower, upper) for lower, upper in zip(hsv_min, hsv_max)])
-
-        binary = np.copy(background_image_hsv)
-        binary = cv.bitwise_not(binary)
-
-        binary = cv.GaussianBlur(binary, (3, 3), 3)
-        return binary
+        hand_only_image = np.astype(img * (mask / 255), np.uint8)
+        return hand_only_image
 
 
-class RandomNoiseBackgroundTransform(RandomBackgroundTransform):
-    def __init__(self, brightness=0.75, contrast=0.3, saturation=.75, hue=0.4):
-        super().__init__(brightness, contrast, saturation, hue)
+class RandomBackground(nn.Module):
+    def __init__(self, background_filter_hsv: tuple[np.ndarray] = (np.array([0, 0, 0]), np.array([180, 5, 5])), *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.background_filter_hsv = background_filter_hsv
 
+    def forward(self, img):
+        img_np = np.array(img)
+        img_with_background = self._apply_background(img_np, self._get_background_filter(img_np) == 255)
+        return Image.fromarray(img_with_background)
+
+    def _apply_background(self, img, mask):
+        raise NotImplementedError("This class is an abstract class and should not be used directly.")
+
+    def _get_background_filter(self, img):
+        """Gets a mask for the background on the image."""
+        return _get_mask(img, self.background_filter_hsv[0], self.background_filter_hsv[1])
+
+
+class RandomBackgroundNoise(RandomBackground):
     def _apply_background(self, img, mask):
         """Applies a random noise background to the image based on the mask."""
 
         noise = np.random.uniform(0, 255, size=img.shape).astype(np.uint8)
-        noise = np.zeros_like(img)
-
-        mask = np.expand_dims(mask, -1)
-        print(mask.shape)
-        print((img * mask / 255).shape)
-        
-        img_with_noise = np.astype(img * (mask / 255) + noise * ((255 - mask) / 255), np.uint8)
-        
-        img_with_noise = cv.GaussianBlur(img_with_noise, (3, 3), 1.5)
-        return img_with_noise
+        img[mask] = noise[mask]
+        return cv.GaussianBlur(img, (3, 3), 1.5)
 
 
-class RandomRealLifeBackgroundTransform(RandomBackgroundTransform):
-    def __init__(self, backgrounds: list[str], brightness=0.75, contrast=0.3, saturation=.75, hue=0.4):
-        super().__init__(brightness, contrast, saturation, hue)
-        self.backgrounds = [cv.imread(background) for background in backgrounds]
+class RandomRealLifeBackground(RandomBackground):
+
+    def __init__(self, backgrounds: list[str], background_filter_hsv: tuple[np.ndarray] = (np.array([0, 0, 0]), np.array([180, 5, 5])), *args, **kwargs):
+        super().__init__(background_filter_hsv, *args, **kwargs)
+        self.backgrounds = [cv.cvtColor(cv.imread(background), cv.COLOR_BGR2RGB) for background in backgrounds]
 
     def _apply_background(self, img, mask):
         """Applies a random real-life background to the image based on the mask."""
+        background_idx = np.random.randint(0, len(self.backgrounds))
+        background = self.backgrounds[background_idx]
 
-        background = np.random.choice(self.backgrounds)
-        background = cv.resize(background, (img.shape[1], img.shape[0]))
-        
-        mask = np.expand_dims(mask, -1)
-
-        img_with_bg_replacement = np.astype(img * (mask / 255) + background * ((255 - mask) / 255), np.uint8)
-        return img_with_bg_replacement
+        background = cv.resize(background, (img.shape[0], img.shape[1]))
+        img[mask] = background[mask]
+        return cv.GaussianBlur(img, (3, 3), 1.5)
